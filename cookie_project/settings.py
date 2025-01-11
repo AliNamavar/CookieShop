3540185219d6b53870c1992831c8511b3e077ca4
AliNamavar <alinamavar315@gmail.com>
2025-01-11 10:32:07 +0330

    change database to postgredb

--- cookie_project/settings.py
+++ cookie_project/settings.py
@@ -83,12 +83,23 @@ WSGI_APPLICATION = 'cookie_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project_db',          # نام دیتابیس
+        'USER': 'postgres',        # نام کاربر
+        'PASSWORD': '123',        # رمز عبور
+        'HOST': 'localhost',           # یا آدرس سرور دیتابیس
+        'PORT': '5432',                # پورت پیش‌فرض PostgreSQL
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
