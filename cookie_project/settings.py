d992fc0f1cda1a1d254e5f0399408e0f2455ea37
AliNamavar <alinamavar315@gmail.com>
2025-02-23 19:08:42 +0330

    .

--- cookie_project/settings.py
+++ cookie_project/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'favorite_module',
     'rest_framework',
     'drf_spectacular',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -136,7 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
